449c77dfdc95f2aaff6009bfebecc8ad4e6bc2c6
carlcarl <carlcarlking@gmail.com>
2024-09-19 14:47:02 +0800

    Add go module and update msg format

--- logger.go
+++ logger.go
@@ -1,10 +1,7 @@
 package ginlogrus
 
 import (
-	"fmt"
-	"math"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -21,11 +18,6 @@ var timeFormat = "02/Jan/2006:15:04:05 -0700"
 
 // Logger is the logrus logger handler
 func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
-	hostname, err := os.Hostname()
-	if err != nil {
-		hostname = "unknow"
-	}
-
 	var skip map[string]struct{}
 
 	if length := len(notLogged); length > 0 {
@@ -41,15 +33,12 @@ func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
 		path := c.Request.URL.Path
 		start := time.Now()
 		c.Next()
-		stop := time.Since(start)
-		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000000.0))
+		latency := time.Since(start)
 		statusCode := c.Writer.Status()
 		clientIP := c.ClientIP()
-		clientUserAgent := c.Request.UserAgent()
-		referer := c.Request.Referer()
-		dataLength := c.Writer.Size()
-		if dataLength < 0 {
-			dataLength = 0
+		bodySize := c.Writer.Size()
+		if bodySize < 0 {
+			bodySize = 0
 		}
 
 		if _, ok := skip[path]; ok {
@@ -57,21 +46,18 @@ func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
 		}
 
 		entry := logger.WithFields(logrus.Fields{
-			"hostname":   hostname,
 			"statusCode": statusCode,
 			"latency":    latency, // time to process
 			"clientIP":   clientIP,
 			"method":     c.Request.Method,
 			"path":       path,
-			"referer":    referer,
-			"dataLength": dataLength,
-			"userAgent":  clientUserAgent,
+			"bodySize":   bodySize,
 		})
 
 		if len(c.Errors) > 0 {
 			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
 		} else {
-			msg := fmt.Sprintf("%s - %s [%s] \"%s %s\" %d %d \"%s\" \"%s\" (%dms)", clientIP, hostname, time.Now().Format(timeFormat), c.Request.Method, path, statusCode, dataLength, referer, clientUserAgent, latency)
+			msg := "API log"
 			if statusCode >= http.StatusInternalServerError {
 				entry.Error(msg)
 			} else if statusCode >= http.StatusBadRequest {
